433206109008127608a82a2c04541fc00e1d10ed
Kimi Wright <ksw38@login01.rc.byu.edu>
2025-05-19 11:40:56 -0600

    updated checkpoints

--- landmarks_syncnet_train_gru2.sh
+++ landmarks_syncnet_train_gru2.sh
@@ -17,4 +17,4 @@ export OMP_NUM_THREADS=$SLURM_CPUS_ON_NODE
 
 # LOAD MODULES, INSERT CODE, AND RUN YOUR PROGRAMS HERE
 mamba activate vsr
-python /home/ksw38/RVL/color_syncnet/Wav2Lip/landmarks_syncnet_train_gru2.py
+python /home/ksw38/RVL/color_syncnet/Wav2Lip/landmarks_syncnet_train_gru2.py --checkpoint_path /home/ksw38/RVL/color_syncnet/Wav2Lip/landmarks_checkpoints_gru2/checkpoint_step000050000.pth
