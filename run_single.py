654ebd0a018ee809d13b4b1442350b49504a852f
DJ <KimiWright64@gmail.com>
2025-07-10 13:41:40 -0400

    run_single

---
+++ run_single.py
@@ -0,0 +1,70 @@
+import os
+import re
+import numpy as np
+from tqdm import tqdm
+
+import torch
+from torch import optim
+import torch.utils.data as data_utils
+
+from hparams import hparams
+from models import SyncNet_landmarks_gru2 as SyncNet # Eventually switch to face only and pregenerated audio
+
+
+data_root = '/home/ksw38/groups/grp_landmarks/nobackup/archive/landmarks_vvadlrs3/main/x_test/'
+ground_truth = '/home/ksw38/groups/grp_landmarks/nobackup/archive/landmarks_vvadlrs3/main/y_test.npy'
+train_data_root = '/home/ksw38/groups/grp_landmarks/nobackup/autodelete/landmarks_vvadlrs3/main/x_train/'
+ground_truth_train = '/home/ksw38/groups/grp_landmarks/nobackup/autodelete/landmarks_vvadlrs3/main/y_train.npy'
+
+checkpoint_dir = 'landmarks_checkpoints_gru2'
+checkpoint_dir = "triplets_checkpoints"
+checkpoint_path = None
+
+def _load(checkpoint_path):
+    use_cuda = torch.cuda.is_available()
+    if use_cuda:
+        checkpoint = torch.load(checkpoint_path)
+    else:
+        checkpoint = torch.load(checkpoint_path,
+                                map_location=lambda storage, loc: storage)
+    return checkpoint
+
+def load_checkpoint(path, model, optimizer, reset_optimizer=False):
+    global global_step
+    global global_epoch
+
+    print("Load checkpoint from: {}".format(path))
+    checkpoint = _load(path)
+    model.load_state_dict(checkpoint["state_dict"])
+    if not reset_optimizer:
+        optimizer_state = checkpoint["optimizer"]
+        if optimizer_state is not None:
+            print("Load optimizer state from {}".format(path))
+            optimizer.load_state_dict(checkpoint["optimizer"])
+    global_step = checkpoint["global_step"]
+    global_epoch = checkpoint["global_epoch"]
+
+    return model
+
+if __name__ == "__main__":
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    shuffle_dataset = False
+    num_workers = 1
+
+    if checkpoint_path  is None:
+        checkpoint_path = os.listdir(checkpoint_dir)[-1]
+        checkpoint_path = os.path.join(checkpoint_dir, checkpoint_path)
+
+    model = SyncNet().to(device)
+    print('total trainable params {}'.format(sum(p.numel() for p in model.parameters() if p.requires_grad)))
+
+    optimizer = optim.Adam([p for p in model.parameters() if p.requires_grad],
+                        lr=hparams.syncnet_lr)
+
+    print("Loading checkpoint path")
+    if checkpoint_path  is None:
+        checkpoint_path = os.listdir(checkpoint_dir)[-1]
+        checkpoint_path = os.path.join(checkpoint_dir, checkpoint_path)
+    gru2.load_checkpoint(checkpoint_path, model, optimizer, reset_optimizer=False)
+    print("Loaded checkpoint from: {}".format(checkpoint_path))
+    model.eval()
