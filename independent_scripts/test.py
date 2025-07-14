75c563ccad727bc7f59fae096d5cc219cc6460bd
Kimi Wright <ksw38@login01.rc.byu.edu>
2025-07-14 14:00:00 -0600

    checked frames

--- independent_scripts/test.py
+++ independent_scripts/test.py
@@ -1,10 +1,13 @@
-import torch
-import torch.nn as nn
+import h5py
 
-m = nn.AdaptiveAvgPool2d((2, 2))
+source_main_path = "/home/ksw38/.cache/kagglehub/datasets/adrianlubitz/vvadlrs3/versions/4/faceImages_small.h5"
+start_frame_num = 0
+syncnet_T = 5
 
-input = torch.randn(1, 3 ,3)
-print(input)
-output = m(input)
-print(output)
-print(output.shape) 
+with h5py.File(source_main_path, 'r') as f:
+    # Get frames from the h5 file
+    x_test = f['x_test']
+
+    for i, frames in enumerate(x_test):
+        frames = frames[start_frame_num:start_frame_num+syncnet_T]
+        print(frames.shape)  # Print the shape of the frames
